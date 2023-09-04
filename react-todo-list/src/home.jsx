dd941dc4735ca1cba2b75a22c96040eaab19596c
Cortrax <aidenrphotos@gmail.com>
2023-09-04 12:20:49 -0400

    Clear input box on button click

--- react-todo-list/src/home.jsx
+++ react-todo-list/src/home.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+
+const taskContainer = document.createElement('div');
+const taskArr = [];
 
 function home() {
-    const [input, setInput] = useState('');
+    const inputRef = useRef();
+
+    let [input, setInput] = useState('');
 
     const [updated, setUpdated] = useState(input);
+
+    const [tasks, addTask] = useState([]);
 
     const handleChange = (event) => {
         setInput(event.target.value);
     };
 
+    taskContainer.className = 'task-container';
+    document.body.appendChild(taskContainer);
+
     const handleClick = () => {
         setUpdated(input);
+        let newDiv = document.createElement('div');
+        newDiv.innerText = input;
+        taskContainer.appendChild(newDiv);
+        setInput('');
     };
 
     return (
@@ -18,6 +32,7 @@ function home() {
             <input
                 type='text'
                 id='text-box'
+                ref={inputRef}
                 value={input}
                 onChange={handleChange}
             />
