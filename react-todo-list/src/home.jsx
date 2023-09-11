aeff2adf3f04cdaa22ff022563b0e78b7461f532
Cortrax <aidenrphotos@gmail.com>
2023-09-11 08:44:03 -0400

    Delete single task on button press

--- react-todo-list/src/home.jsx
+++ react-todo-list/src/home.jsx
@@ -1,62 +1,48 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useReducer } from 'react';
 import DelIcon from './assets/close-btn.png';
 import './styling/home.scss';
-
-let taskArr = [];
 
 function Task(todo) {
     this.todo = todo;
 }
 
 function home() {
+    const [tasks, setTasks] = useState([]);
     const inputRef = useRef();
 
-    const [deleteTask, setDelete] = useState(false);
-
-    const taskContainer = document.createElement('div');
-    taskContainer.className = 'task-container';
-    document.body.appendChild(taskContainer);
-
-    let taskDiv = document.querySelector('.task-div');
-
-    const handleDel = () => {
-        for (let i = 0; i < taskArr.length; i++) {
-            taskContainer.remove(taskDiv);
-            taskArr.splice(taskDiv, 1);
-        }
+    const handleDelete = (index) => {
+        const updatedTask = [...tasks];
+        updatedTask.splice(index, 1);
+        setTasks(updatedTask);
     };
-
-    console.log(deleteTask);
 
     const handleClick = () => {
-        let todo = inputRef.current.value;
-        let newTask = new Task(todo);
-        taskArr.push(newTask);
-        console.log(taskArr);
+        const todo = inputRef.current.value;
+        const newTask = new Task(todo);
+        setTasks([...tasks, newTask]);
         inputRef.current.value = '';
-
-        if (taskArr[taskArr.length - 1] !== '') {
-            const taskDiv = document.createElement('div');
-            taskDiv.className = 'task-div';
-
-            const taskText = document.createElement('p');
-            const deleteBtn = document.createElement('button');
-
-            taskText.innerText = taskArr[taskArr.length - 1].todo;
-            taskContainer.appendChild(taskDiv);
-            taskDiv.appendChild(taskText);
-            taskDiv.appendChild(deleteBtn);
-            taskContainer.appendChild(taskDiv);
-
-            deleteBtn.innerHTML = `<img src=${DelIcon} alt='delete-btn' />`;
-            deleteBtn.className = 'delete-btn';
-            deleteBtn.onclick = handleDel;
-        }
     };
+
     return (
-        <div id='home-container'>
-            <input type='text' ref={inputRef} maxLength={40} />
-            <button onClick={handleClick}>Add</button>
+        <div>
+            <div id='input-container'>
+                <input type='text' ref={inputRef} maxLength={40} />
+                <button onClick={handleClick}>Add</button>
+            </div>
+
+            <div id='task-container'>
+                {tasks.map((task, index) => (
+                    <div id='task-div' key={index}>
+                        <p>{task.todo}</p>
+                        <button
+                            id='delete-btn'
+                            onClick={() => handleDelete(index)}
+                        >
+                            <img src={DelIcon} alt='delete-btn' />
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
