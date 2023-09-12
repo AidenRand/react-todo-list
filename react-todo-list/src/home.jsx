48a3b4752638d188a4c040b9b655f564c769e345
Cortrax <aidenrphotos@gmail.com>
2023-09-12 16:28:57 -0400

    Add check button

--- react-todo-list/src/home.jsx
+++ react-todo-list/src/home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useReducer } from 'react';
-import DelIcon from './assets/close-btn.png';
+import DelIcon from './assets/close-icon.png';
+import CheckIcon from './assets/check-icon.png';
 import './styling/home.scss';
 
 function Task(todo) {
@@ -8,6 +9,7 @@ function Task(todo) {
 
 function home() {
     const [tasks, setTasks] = useState([]);
+    const [complete, setComplete] = useState(false);
     const inputRef = useRef();
 
     const handleDelete = (index) => {
@@ -16,29 +18,40 @@ function home() {
         setTasks(updatedTask);
     };
 
+    const handleComplete = (index) => {};
+
     const handleClick = () => {
-        const todo = inputRef.current.value;
-        const newTask = new Task(todo);
-        setTasks([...tasks, newTask]);
-        inputRef.current.value = '';
+        if (inputRef.current.value !== '') {
+            const todo = inputRef.current.value;
+            const newTask = new Task(todo);
+            setTasks([...tasks, newTask]);
+            inputRef.current.value = '';
+        } else {
+            alert('Input cannot be empty');
+        }
     };
+
+    console.log([...tasks]);
 
     return (
         <>
             <div id='input-container'>
-                <input type='text' ref={inputRef} maxLength={40} />
+                <input type='text' ref={inputRef} maxLength={80} />
                 <button onClick={handleClick}>Add</button>
             </div>
 
             <div id='task-container'>
                 {tasks.map((task, index) => (
                     <div id='task-div' key={index}>
-                        <p>{task.todo}</p>
+                        <p ref={taskTextRef}>{task.todo}</p>
                         <button
                             id='delete-btn'
                             onClick={() => handleDelete(index)}
                         >
                             <img src={DelIcon} alt='delete-btn' />
+                        </button>
+                        <button id='check-btn'>
+                            <img src={CheckIcon} alt='check-btn' />
                         </button>
                     </div>
                 ))}
